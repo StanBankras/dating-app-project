32a868ed20a5204e09c983661ef5122157d029db
Stan <Stanbankras@gmail.com>
2020-02-20 10:22:43 +0100

    Dynamic content rendering with EJS

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -3,7 +3,30 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { });
+  let blogPosts = [
+    {
+        title: 'Perk is for real!',
+        body: '...',
+        author: 'Aaron Larner',
+        publishedAt: new Date('2016-03-19'),
+        createdAt: new Date('2016-03-19')
+    },
+    {
+        title: 'Development continues...',
+        body: '...',
+        author: 'Aaron Larner',
+        publishedAt: new Date('2016-03-18'),
+        createdAt: new Date('2016-03-18')
+    },
+    {
+        title: 'Welcome to Perk!',
+        body: '...',
+        author: 'Aaron Larner',
+        publishedAt: new Date('2016-03-17'),
+        createdAt: new Date('2016-03-17')
+    }
+];
+  res.render('index', { posts: blogPosts });
 });
 /* GET about page. */
 router.get('/about', function(req, res, next) {
