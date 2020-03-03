60a6c4992b8fb49fe26c844e2a5242ea06993339
Stan <Stanbankras@gmail.com>
2020-03-03 17:26:56 +0100

    Viewed matches history fully operational

--- feature/working_feature/public/javascripts/match-slider.js
+++ feature/working_feature/public/javascripts/match-slider.js
@@ -1,24 +1,34 @@
+// Grab elements from HTML
 const matchSlider = document.querySelector('#match-items');
 const matchSliderItems = document.querySelector('#match-items ul');
 const fwdButton = document.querySelector('#slide-forward');
 const backButton = document.querySelector('#slide-back');
 const slideIcons = document.querySelectorAll('.slider-nav .slide-icon');
-let listOffset = 0;
-const step = 364;
+const viewedMatchesList = document.querySelector('#viewed-matches ul');
+
+// Icons
 const filledDot = 'fiber_manual_record';
 const emptyDot = 'panorama_fish_eye';
 
+// Defaults for the slider
+const step = 364;
+let listOffset = 0;
+let viewedMatches = [];
+
+// Hide scrollbar with Javascript
 matchSlider.style.overflow = 'hidden';
 
-// Set first icon to selected
-document.querySelector('.slider-nav .material-icons[data-slide="0"]').innerHTML = 'fiber_manual_record';
+// Set first icon to selected and add this match to the viewed-matches list
+document.querySelector('.slider-nav .material-icons[data-slide="0"]').innerHTML = filledDot;
+addViewedMatch(0);
 
 // Event listener for the forward button
 fwdButton.addEventListener('click', () => {
   if(Math.abs(listOffset)/step >= matches.length-1) {
     return;
   }
-  removeSliderIcon((Math.abs(listOffset)/step));
+  removeSliderIcon(Math.abs(listOffset)/step);
+  addViewedMatch(Math.abs(listOffset)/step);
   listOffset -= step;
   matchSliderItems.style.transform = `translateX(${ listOffset }px)`;
   setSliderIcon(Math.abs(listOffset)/step);
@@ -29,7 +39,8 @@ backButton.addEventListener('click', () => {
   if(listOffset == 0) {
     return;
   }
-  removeSliderIcon((Math.abs(listOffset)/step));
+  removeSliderIcon(Math.abs(listOffset)/step);
+  addViewedMatch(Math.abs(listOffset)/step);
   listOffset += step;
   matchSliderItems.style.transform = `translateX(${ listOffset }px)`;
   setSliderIcon(Math.abs(listOffset)/step);
@@ -43,6 +54,7 @@ slideIcons.forEach((slide) => {
     // If the active slide is being clicked, return
     if (slide == prevSlide) return;
 
+    addViewedMatch(Math.abs(listOffset)/step);
     listOffset = -slide * step;
     matchSliderItems.style.transform = `translateX(${ listOffset }px)`;
     removeSliderIcon(prevSlide);
@@ -61,3 +73,43 @@ function removeSliderIcon(sliderStep) {
   document.querySelector(`.slider-nav .material-icons[data-slide="${ sliderStep }"]`).innerHTML = emptyDot;
   return;
 }
+
+// Create a new list item that can be appended to viewed-matches (higher-order function: addViewedMatch())
+function createViewedMatch(match) {
+  let li = document.createElement('li');
+  // Set data attribute so it the list-item can be associated with the same slider item
+  li.setAttribute('data-match', match)
+  li.innerHTML = `
+  <figure>
+    <img
+      src="${ matches[match].picture }"
+      alt="Dating person"
+      >
+    <figcaption>Mark de Jong, 26</figcaption>
+  </figure>
+  `
+  return li;
+
+}
+
+// Adds a viewed match to the viewed-matches list
+function addViewedMatch(match) {
+  if(viewedMatches.includes(match)) return;
+  viewedMatches.push(match);
+  return viewedMatchesList.appendChild(createViewedMatch(match));
+}
+
+// Add eventlistener to the list of viewed matches, to catch clicks and set move the slider accordingly
+viewedMatchesList.addEventListener('click', (e) => {
+  if(e.target && e.target.nodeName == "LI") {
+    matchNr = e.target.dataset.match;
+    // If clicked item == current active match, return
+    if(matchNr == Math.abs(listOffset)/step) return;
+
+    removeSliderIcon(Math.abs(listOffset)/step);
+    addViewedMatch(Math.abs(listOffset)/step);
+    listOffset = -matchNr * step;
+    matchSliderItems.style.transform = `translateX(${ listOffset }px)`;
+    setSliderIcon(matchNr);
+  }
+});
