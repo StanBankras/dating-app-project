3f795c4aa40ce4465557f1297a0a6430fffa97df
Stan <Stanbankras@gmail.com>
2020-03-03 16:09:51 +0100

    Matches slider is 100% operational

--- feature/working_feature/public/javascripts/match-slider.js
+++ feature/working_feature/public/javascripts/match-slider.js
@@ -4,7 +4,7 @@ const matchSlider = document.querySelector('#match-items');
 const matchSliderItems = document.querySelector('#match-items ul');
 const fwdButton = document.querySelector('#slide-forward');
 const backButton = document.querySelector('#slide-back');
-const slideIcons = document.querySelector('.slider-nav .material-icons');
+const slideIcons = document.querySelectorAll('.slider-nav .slide-icon');
 let listOffset = 0;
 const step = 364;
 
@@ -13,9 +13,9 @@ matchSlider.style.overflow = 'hidden';
 // Set first icon to selected
 document.querySelector('.slider-nav .material-icons[data-slide="0"]').innerHTML = 'fiber_manual_record';
 
+// Event listeners for the backward and forward button
 fwdButton.addEventListener('click', () => {
   if(Math.abs(listOffset)/step >= matches.length-1) {
-    console.log('done');
     return;
   }
   listOffset -= step;
@@ -32,10 +32,33 @@ backButton.addEventListener('click', () => {
   setSliderIcon(false);
 });
 
+slideIcons.forEach((slide) => {
+  slide.addEventListener('click', (e) => {
+    let prevSlide = Math.abs(listOffset)/step;
+    let slide = e.target.dataset.slide;
+    listOffset = -slide * step;
+    matchSliderItems.style.transform = `translateX(${ listOffset }px)`;
+    removeSliderIcon(prevSlide);
+
+    if(prevSlide > slide) {
+      setSliderIcon(false);
+    } else {
+      setSliderIcon(true);
+    }
+  });
+})
+
+// Fill the icon of the active match (true for direction = forward move)
 function setSliderIcon(direction) {
-  if(direction) { document.querySelector(`.slider-nav .material-icons[data-slide="${ Math.abs(listOffset) / step - 1 }"]`).innerHTML = 'panorama_fish_eye'; }
-  else { document.querySelector(`.slider-nav .material-icons[data-slide="${ Math.abs(listOffset) / step + 1 }"]`).innerHTML = 'panorama_fish_eye'; }
+  if(direction) { removeSliderIcon(Math.abs(listOffset) / step - 1); }
+  else { removeSliderIcon(Math.abs(listOffset) / step + 1); }
 
   let slideNow = document.querySelector(`.slider-nav .material-icons[data-slide="${ Math.abs(listOffset) / step }"]`);
   slideNow.innerHTML = 'fiber_manual_record';
+  return;
+}
+
+function removeSliderIcon(icon) {
+  document.querySelector(`.slider-nav .material-icons[data-slide="${ icon }"]`).innerHTML = 'panorama_fish_eye';
+  return;
 }
