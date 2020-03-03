0a8d340ba87ce3a930a4b64fbabe5bb568068795
Stan <Stanbankras@gmail.com>
2020-03-03 15:56:13 +0100

    Adding functionality to the slider icons

--- feature/working_feature/public/javascripts/match-slider.js
+++ feature/working_feature/public/javascripts/match-slider.js
@@ -4,21 +4,38 @@ const matchSlider = document.querySelector('#match-items');
 const matchSliderItems = document.querySelector('#match-items ul');
 const fwdButton = document.querySelector('#slide-forward');
 const backButton = document.querySelector('#slide-back');
+const slideIcons = document.querySelector('.slider-nav .material-icons');
 let listOffset = 0;
+const step = 364;
 
 matchSlider.style.overflow = 'hidden';
 
+// Set first icon to selected
+document.querySelector('.slider-nav .material-icons[data-slide="0"]').innerHTML = 'fiber_manual_record';
+
 fwdButton.addEventListener('click', () => {
-  listOffset -= 100;
+  if(Math.abs(listOffset)/step >= matches.length-1) {
+    console.log('done');
+    return;
+  }
+  listOffset -= step;
   matchSliderItems.style.transform = `translateX(${ listOffset }px)`;
+  setSliderIcon(true);
 });
 
 backButton.addEventListener('click', () => {
   if(listOffset == 0) {
     return;
   }
-  listOffset += 100;
+  listOffset += step;
   matchSliderItems.style.transform = `translateX(${ listOffset }px)`;
+  setSliderIcon(false);
 });
 
-console.log(fwdButton);
+function setSliderIcon(direction) {
+  if(direction) { document.querySelector(`.slider-nav .material-icons[data-slide="${ Math.abs(listOffset) / step - 1 }"]`).innerHTML = 'panorama_fish_eye'; }
+  else { document.querySelector(`.slider-nav .material-icons[data-slide="${ Math.abs(listOffset) / step + 1 }"]`).innerHTML = 'panorama_fish_eye'; }
+
+  let slideNow = document.querySelector(`.slider-nav .material-icons[data-slide="${ Math.abs(listOffset) / step }"]`);
+  slideNow.innerHTML = 'fiber_manual_record';
+}
