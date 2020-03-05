94f832a11a1990f7679c22bcd8bcdbb29fffa41a
Stan <Stanbankras@gmail.com>
2020-03-05 19:28:07 +0100

    Changed /movies route into the main / route

--- feature/practice_project/routes/index.js
+++ feature/practice_project/routes/index.js
@@ -2,32 +2,22 @@ const express = require('express');
 const router = express.Router();
 const path = require('path');
 
+
+let movies = [
+  {
+      title: 'Titanic',
+      description: 'This is the titanic',
+  },
+  {
+      title: 'Titanic2',
+      description: 'This is the titanic2',
+  }
+];
+
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  let blogPosts = [
-    {
-        title: 'Perk is for real!',
-        body: '...',
-        author: 'Aaron Larner',
-        publishedAt: new Date('2016-03-19'),
-        createdAt: new Date('2016-03-19')
-    },
-    {
-        title: 'Development continues...',
-        body: '...',
-        author: 'Aaron Larner',
-        publishedAt: new Date('2016-03-18'),
-        createdAt: new Date('2016-03-18')
-    },
-    {
-        title: 'Welcome to Perk!',
-        body: '...',
-        author: 'Aaron Larner',
-        publishedAt: new Date('2016-03-17'),
-        createdAt: new Date('2016-03-17')
-    }
-];
-  res.render('index', { posts: blogPosts });
+  res.render('movies', { movies: movies });
 });
 
 /* GET about page. */
@@ -50,17 +40,6 @@ router.get('/add', (req, res, next) => {
   res.render('add', { });
 });
 
-let movies = [
-  {
-      title: 'Titanic',
-      description: 'This is the titanic',
-  },
-  {
-      title: 'Titanic2',
-      description: 'This is the titanic2',
-  }
-];
-
 /* GET movies page. */
 router.get('/movies', (req, res, next) => {
   res.render('movies', { movies: movies }) 
@@ -73,7 +52,7 @@ router.post('/', (req, res) => {
     title: title,
     description: description
   });
-  res.redirect('movies');
+  res.redirect('/');
 });
 
 // Receives post requests from a form to edit movie title or movie description
@@ -82,19 +61,19 @@ router.post('/edit', (req, res) => {
     const newTitle = req.body.newTitle;
     const movieNr = req.body.movieNr;
     movies[movieNr].title = newTitle;
-    res.redirect('movies');
+    res.redirect('/');
   } else if (req.body.newDescription) {
     const newDescription = req.body.newDescription;
     const movieNr = req.body.movieNr;
     movies[movieNr].description = newDescription;
-    res.redirect('movies');
+    res.redirect('/');
   }
 });
 
 router.post('/delete', (req, res) => {
   const movieNr = req.body.movieNr;
   movies.splice(movieNr, 1);
-  res.redirect('movies');
+  res.redirect('/');
 });
 
 router.get("/mp3", (req, res, next) => {
