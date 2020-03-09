fbe392be6dcf54b28c0637ad3efa81e9d012b971
Stan <Stanbankras@gmail.com>
2020-03-09 20:06:05 +0100

    Added slug to formdata and all promises are now async await (cleaner code)

--- feature/practice_project/routes/index.js
+++ feature/practice_project/routes/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const path = require('path');
 const mongo = require('mongodb');
+const slug = require('slug');
 const ObjectID = mongo.ObjectID;
 
 // Load environment variables
@@ -48,50 +49,57 @@ router.get('/add', (req, res, next) => {
 });
 
 // Post route for adding a new movie to the MongoDB
-router.post('/', (req, res) => {
-  const title = req.body.title;
-  const description = req.body.description;
-  
-  db.collection('movies').insertOne({
-    title: title,
-    description: description
-  }).catch(err => { console.log(err) });
+router.post('/', async (req, res) => {
+  const title = slug(req.body.title);
+  const description = slug(req.body.description);
+  try {
+    await db.collection('movies').insertOne({
+      title: title,
+      description: description
+    })
+  } catch(err) {
+    console.error(error);
+  }
 
   res.redirect('/');
 });
 
 // Receives post requests from a form to edit movie title or movie description
-router.post('/edit', (req, res) => {
+router.post('/edit', async (req, res) => {
   if (req.body.newTitle) {
-    const newTitle = req.body.newTitle;
-    const movieNr = req.body.movieNr;
+    const newTitle = slug(req.body.newTitle);
+    const movieNr = slug(req.body.movieNr);
     // Update title in database
-    db.collection('movies').updateOne(
-      { _id: ObjectID(movieNr) },
-      { $set: { "title": newTitle } }
-    )
-    .then((response) => console.log(response.modifiedCount))
-    .catch(err => { console.log(err) });
+    try {
+      await db.collection('movies').updateOne(
+        { _id: ObjectID(movieNr) },
+        { $set: { "title": newTitle } }
+      )
+    } catch(err) {
+      console.error(error);
+    }
 
     res.redirect('/')
 
   } else if (req.body.newDescription) {
-    const newDescription = req.body.newDescription;
-    const movieNr = req.body.movieNr;
+    const newDescription = slug(req.body.newDescription);
+    const movieNr = slug(req.body.movieNr);
     // Update description in database
-    db.collection('movies').updateOne(
-      { _id: ObjectID(movieNr) },
-      { $set: { "description": newDescription } }
-    )
-    .then((response) => console.log(response.modifiedCount))
-    .catch(err => { console.log(err) });
+    try {
+      await db.collection('movies').updateOne(
+        { _id: ObjectID(movieNr) },
+        { $set: { "description": newDescription } }
+      )
+    } catch(err) {
+      console.error(error);
+    }
 
     res.redirect('/')
   }
 });
 
 router.post('/delete', async (req, res) => {
-  const movieNr = req.body.movieNr;
+  const movieNr = slug(req.body.movieNr);
   // Delete movie by _id
   try {
     await db.collection('movies').deleteOne({ _id: ObjectID(movieNr) })
